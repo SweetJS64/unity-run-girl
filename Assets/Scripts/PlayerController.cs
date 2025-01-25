377c08a5dd5b1fb188d637c1f385ce4a866fec8b
SweetJS64 <miroran64@gmail.com>
2025-01-25 16:11:14 +0400

    Add PlayerControler and method Fly

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float FlyVelocity = 6f;
+    
+    private Rigidbody2D _rb;
+    void Start()
+    {
+        Init();
+    }
+    
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Fly();
+        }
+    }
+
+    private void Init()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Fly()
+    {
+        _rb.velocity = new Vector2(0, FlyVelocity);
+    }
+}
