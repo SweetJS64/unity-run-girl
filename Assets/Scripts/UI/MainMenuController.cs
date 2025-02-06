c0d282630996aef7ed9691518a05f6d01947c061
SweetJS64 <miroran64@gmail.com>
2025-02-07 01:48:47 +0400

    Upgrade magic effect/ add UI buttons

---
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+namespace UI
+{
+    public class MainMenuController : MonoBehaviour
+    {
+        [SerializeField] private float SpeedMove = 0.2f;
+        
+        private Camera _cameraMain;
+        private Vector3 _startPos = Vector3.zero;
+        private Vector3 _closePos;
+        private bool _menuDisable;
+        
+        private void Start()
+        {
+            Init();
+        }
+        
+        private void Update()
+        {
+            if (!_menuDisable) return;
+            CloseMainMenu();
+        }
+
+        private void Init()
+        {
+            _cameraMain = Camera.main;
+            if (_cameraMain == null)
+            {
+                Debug.LogError("_cameraMain == null");
+                return;
+            }
+            _closePos = _cameraMain.ViewportToWorldPoint(new Vector3(0.5f, 1f, 0)) * 2;
+        }
+        
+        public void StartGame()
+        {
+            _menuDisable = true;
+        }
+
+        private void CloseMainMenu()
+        {
+            transform.position = new Vector3(
+                _closePos.x, 
+                Mathf.Lerp(transform.position.y, _closePos.y, SpeedMove * Time.deltaTime), 
+                0);
+        }
+    }
+}
